41e4ca5673297f9ac9c9cb93a238d01b3909a1f8
AndileSouth <zondoandile97@gmail.com>
2023-08-11 15:23:27 +0100

    added type of Activity feature

--- src/Activity.js
+++ src/Activity.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './Activity.css'
 
-async function fetchData() {
+
+// 1. this functions gets the full object but only works when invoked by the other
+
+async function fetchData(type) {
     try {
-      const response = await fetch('https://www.boredapi.com/api/activity/');
+      // Use different URLs based on the selected type
+      const apiUrl = type === 'random'
+      ? 'https://www.boredapi.com/api/activity/'
+      : `https://www.boredapi.com/api/activity?type=${type}`;
+
+
+      const response = await fetch(apiUrl);
       const jsonData = await response.json();
       return jsonData;
     } catch (error) {
@@ -13,12 +22,22 @@ async function fetchData() {
   }
   
   function Activity() {
+
     const [data, setData] = useState(null);
     const [activity, setActivity] = useState(null);
+
+    // 1. [filter by type]
+    const [selectedType, setSelectedType] = useState('random'); // first type is random
+
   
+    // 2. the useEffect will invoke fetchData() and use it as jsondata variable
     useEffect(() => {
       async function fetchDataAndSetState() {
-        const jsonData = await fetchData();
+
+        // 3. jsonData now has the data returned in the fetcheddata()
+        const jsonData = await fetchData(selectedType);
+
+        // 4. if the fetch is successful the setData and setActivity is updated
         if (jsonData) {
           setData(jsonData);
           setActivity(jsonData.activity);
@@ -29,51 +48,75 @@ async function fetchData() {
       fetchDataAndSetState();
     }, []);
 
+    // [generate] 1.2 this uses the btn to get invoked
     async function generate() {
-        const jsonData = await fetchData(); 
+      // 2. we get the jsonData just like before
+        const jsonData = await fetchData(selectedType); 
+        // 3. if the fetch is successful we update the setActivity to the new string value
         if (jsonData) {
             setData(jsonData);
             setActivity(jsonData.activity);
             console.log(jsonData);
-          };
+          };//
     };
     
+
+    // 2. [filter by type]
+     // Function to handle radio button change
+    function handleTypeChange(event) {
+      setSelectedType(event.target.value);
+      console.log(selectedType);
+    }
   
     return (
       <div className="Activity">
         <div className="content">
-          <div className="activity-box">{activity}</div>
-          <button className='generate' onClick={generate}>Generate Activity</button>
+          <div className="activity-box">{
+          // 5. the Activity is used in the dom
+          activity}</div>
+          <button className='generate' onClick={
+            //1.1. button invokes function
+            generate}>Generate Activity</button>
 
         <div>
-          <input type="radio" name='type' value="random"  id='random'/>
+          <input type="radio" name='type' value="random"  id='random' checked={selectedType === 'random'}
+            onChange={handleTypeChange}/>
           <label htmlFor="random"> random</label>
 
-          <input type="radio" name='type' value="social"  id='social'/>
+          <input type="radio" name='type' value="social"  id='social' checked={selectedType === 'social'}
+            onChange={handleTypeChange}/>
           <label htmlFor="social"> social</label>
 
-          <input type="radio" name='type' value="education"  id='education'/>
+          <input type="radio" name='type' value="education"  id='education' checked={selectedType === 'education'}
+            onChange={handleTypeChange}/>
           <label htmlFor="education"> education</label>
 
-          <input type="radio" name='type' value="recreational"  id='recreational'/>
+          <input type="radio" name='type' value="recreational"  id='recreational' checked={selectedType === 'recreational'}
+            onChange={handleTypeChange}/>
           <label htmlFor="recreational"> recreational</label>
 
-          <input type="radio" name='type' value="diy"  id='diy'/>
+          <input type="radio" name='type' value="diy"  id='diy' checked={selectedType === 'diy'}
+            onChange={handleTypeChange}/>
           <label htmlFor="diy"> diy</label>
 
-          <input type="radio" name='type' value="charity"  id='charity'/>
+          <input type="radio" name='type' value="charity"  id='charity' checked={selectedType === 'charity'}
+            onChange={handleTypeChange}/>
           <label htmlFor="charity"> charity</label>
 
-          <input type="radio" name='type' value="cooking"  id='cooking'/>
+          <input type="radio" name='type' value="cooking"  id='cooking' checked={selectedType === 'cooking'}
+            onChange={handleTypeChange}/>
           <label htmlFor="cooking"> cooking</label>
 
-          <input type="radio" name='type' value="relaxation"  id='relaxation'/>
+          <input type="radio" name='type' value="relaxation"  id='relaxation' checked={selectedType === 'type'}
+            onChange={handleTypeChange}/>
           <label htmlFor="relaxation"> relaxation</label>
 
-          <input type="radio" name='type' value="music"  id='music'/>
+          <input type="radio" name='type' value="music"  id='music' checked={selectedType === 'music'}
+            onChange={handleTypeChange}/>
           <label htmlFor="music"> music</label>
 
-          <input type="radio" name='type' value="busywork"  id='busywork'/>
+          <input type="radio" name='type' value="busywork"  id='busywork' checked={selectedType === 'bustywork'}
+            onChange={handleTypeChange}/>
           <label htmlFor="busywork"> busywork</label>
 
           </div>
